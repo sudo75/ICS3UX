6e357ecee69ae46497ba1ec0e370096552c72fa4
Eric Skaftason <skaftasoneric@gmail.com>
2026-01-11 22:13:01 -0500

    cleaned up code

--- Unit4Assignment/Unit4Assignment_Question4/Main.java
+++ Unit4Assignment/Unit4Assignment_Question4/Main.java
@@ -1,3 +1,10 @@
+/*
+Eric Skaftason
+Program 4 - get total for food order
+2026-01-12
+ICS3UX
+*/
+
 import java.text.DecimalFormat; // import DecimalFormat class from java.text
 import java.util.*; // import all classes from the java.util package
 public class Main{ // declare main class
@@ -27,42 +34,38 @@ public class Main{ // declare main class
         int orderNumber = 1; // declare new int and init to 1
 
         while (true) { // run the loop indefinately until break statement
-            double orderTotal = 0; // declare an init a new double
             System.out.println("Order #" + orderNumber + '\n'); // prin the order number
-            System.out.println("Enter -1 to end order at any time.\n");
+            System.out.println("Enter -1 to end order at any time.\n"); // print the 
 
             System.out.print("Number of hamburgers: "); // print a prompt for num of hamburgers
             numHamburgers = input.nextInt(); // get num of hamburgers by getting the next token as an int  
             if (numHamburgers == -1) break; // if -1 is entered break out of the loop
-            orderTotal += numHamburgers * hamburger.getPrice(); // add the item price x quantity to the order total 
+            total += numHamburgers * hamburger.getPrice(); // add the item price x quantity to the order total
             System.out.println("Each hamburger has " + hamburger.toString() + '\n'); // print nutrition info for a hamburger
             
             System.out.print("Number of salads: "); // prompt for num of salads
             numSalads = input.nextInt(); // get num of salads by getting the next token as an int
             if (numSalads == -1) break; // if -1 is entered break out of the loop
-            orderTotal += numSalads * salad.getPrice(); // add the item price x quantity to the order total
+            total += numSalads * salad.getPrice(); // add the item price x quantity to the order total
             System.out.println("Each salad has " + salad.toString() + '\n'); // print nutrition info for a salad
 
-            System.out.print("Number of nuggets: "); // prompt for num of orders of nuggets
+            System.out.print("Number of nugget orders (6pc): "); // prompt for num of orders of nuggets
             numNuggets = input.nextInt(); // get num of nugget orders by getting the next token as an int
             if (numNuggets == -1) break; // if -1 is entered break out of the loop
-            orderTotal += numNuggets * nuggets.getPrice(); // add the item price x quantity to the order total
+            total += numNuggets * nuggets.getPrice(); // add the item price x quantity to the order total
             System.out.println("Each order of nuggets has " + nuggets.toString() + '\n'); // print nutrition info for an order of nuggets
 
             System.out.print("Number of sodas: "); // prompt for num of sodas
             numSodas = input.nextInt(); // get num of sodas by getting the next token as an int
             if (numSodas == -1) break; // if -1 is entered break out of the loop
-            orderTotal += numSodas * soda.getPrice(); // add the item price x quantity to the order total
+            total += numSodas * soda.getPrice(); // add the item price x quantity to the order total
             System.out.println("Each soda has " + soda.toString() + '\n'); // print nutrition info for a soda
-
-
-            orderTotal *= HST_PRICE_FACTOR; // calcuate the order total with tax
-            total += orderTotal; // add this order total to the running grand total
 
             // Increment order number for next order
             orderNumber++;
         }
 
+        total *= HST_PRICE_FACTOR;
         System.out.println("\nYour grand total with all orders with tax comes to $" + df.format(total)); // print the total for all orders
 	
 	} // close main method
